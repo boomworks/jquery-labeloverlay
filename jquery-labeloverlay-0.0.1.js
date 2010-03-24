3e9f859504509a2b0eb0de72d551b4ed820e0ece
Lindsay Evans <linz@linz.id.au>
2010-03-24 15:53:03 +1100

    Adding comments

--- jquery-labeloverlay-0.0.1.js
+++ jquery-labeloverlay-0.0.1.js
@@ -9,6 +9,7 @@
 
 	$.fn.labelOverlay = function(opts){
 
+		// Merge user supplied options with defaults
 		var options = $.extend({}, $.fn.labelOverlay.defaults, opts);
 
 		this.each(function(){
@@ -17,12 +18,17 @@
 					$lbl = $this.siblings(options.labelSelector)
 			;
 
+			// Bind various events to target field
+			// TODO: will live() work here? do we need it?
 			$this
 				.bind('now focus keyup change blur', function(e){
+					// Choose speed & opacity based on event type
 					var speed = e.type === 'focus' ? options.focusFadeSpeed : e.type === 'keyup' ? options.typeFadeSpeed : options.blurFadeSpeed,
 							opacity = e.type === 'focus' || e.type === 'keyup' ? options.opacity : options.startOpacity;
+					// Do the fade
 					fade($lbl, $this.val(), speed, opacity);
 				})
+				// Trigger now event to fade pre-filled/focused fields
 				.trigger('now')
 			;
 
@@ -31,10 +37,12 @@
 		return this;
 	};
 
+	// either fade to desired opacity or hide, depending on field value
 	fade = function($obj, val, speed, opacity){
 		$.trim(val) === '' ? $obj.show().fadeTo(speed, opacity) : $obj.fadeTo(0, 0).hide();
 	};
 
+	// Default settings
 	$.fn.labelOverlay.defaults = {
 		opacity: 0.5,
 		startOpacity: 1,
